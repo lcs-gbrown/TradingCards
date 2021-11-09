d118bd48725da968c756410a941677dacc8ae53f
gabi brown <gbrown@lcs.on.ca>
2021-11-09 11:49:01 -0500

    added values for each playing card, need to change the images

--- TradingCards/Model/TradingCard.swift
+++ TradingCards/Model/TradingCard.swift
@@ -37,6 +37,49 @@ TradingCard(imageOne: "Scottie Pippen", playerName: "Scottie Pippen",
             AST: "5.2",
             caption: "Scottie is also a renowned player. Him and Micheal Jordan were often nicknames Batman and Robin as they were the dynamic duo."),
 
+TradingCard(imageOne: "Dennis Rodman", playerName: "Dennis Rodman",
+            position: "PF",
+            G: "911",
+            PTS: "7.3",
+            TRB: "13.1",
+            AST: "1.8",
+            caption: "Dennis Rodman was the star defensive player of the bulls. He was part of a trio with Michael and Scottie, but often was a little excluded. He was very wild in comparison to the other two."),
+
+TradingCard(imageOne: "Steve Kerr", playerName: "Steve Kerr",
+            position: "PG",
+            G: "910",
+            PTS: "6.0",
+            TRB: "1.2",
+            AST: "1.8",
+            caption: "Steve was a point guard on the Bulls, and widely known as a really nice guy, but a bit of an underdog."),
+
+TradingCard(imageOne: "Tony Kukoc", playerName: "Tony Kukoc",
+            position: "SF, PF",
+            G: "846",
+            PTS: "11.6",
+            TRB: "4.2",
+            AST: "3.7",
+            caption: "Tony was a bit of an outsider to the team. He came to play in America from Croatia, and a few others felt threatened by him. They didn't like him coming in and changing things- but overall he was very talented and everyone learned to deal."),
+
+TradingCard(imageOne: "Ron Harper", playerName: "Ron Harper",
+            position: "SG, PG",
+            G: "1009",
+            PTS: "13.1",
+            TRB: "4.3",
+            AST: "3.9",
+            caption: "Ron Harper actually started playing against the bulls on the Cleveland Cavaliers, and was seen as some of Michael Jordan's biggest competition. He did have a few injuries throughout his career, and by the time he came to the Bulls he was more of a defensive player."),
+
+TradingCard(imageOne: "Phil Jackson", playerName: "Phil Jackson",
+            position: "coach",
+            G: "n/a",
+            PTS: "n/a",
+            TRB: "n/a",
+            AST: "n/a",
+            caption: "Phil Jackson was the coach that lead the Bulls to so many victories. He really saw Michael's talent and played it. He knew how to us ethe teams advantages, but sometimes ha dconflict because of how much attention he paid to MJ."),
+
+
+
+
 
 
 
