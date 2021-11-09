61fe85b4678d758416f6703b840a1a4656de57bb
gabi brown <gbrown@lcs.on.ca>
2021-11-08 19:38:18 -0500

    created variables so can autofill variables

--- TradingCards/ContentView.swift
+++ TradingCards/ContentView.swift
@@ -7,42 +7,50 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct TradingCard: View {
+    let imageOne: String
+    let playerName: String
+    let Pos: String
+    let G: String
+    let PTS: String
+    let TRB: String
+    let AST: String
+    let caption: String
+    
     var body: some View {
-        VStack {
-           
-            ZStack {
-                //Layer 1
-                Color.red
-                //Layer 2
-                Color.black
-                    .padding(10)
+            VStack {
                 
-                Image("Michael Jordan")
-                    .resizable()
-                    .scaledToFit()
-                    .padding(10)
+                ZStack {
+                    //Layer 1
+                    Color.red
+                    //Layer 2
+                    Color.black
+                        .padding(10)
+                    
+                    Image(imageOne)
+                        .resizable()
+                        .scaledToFit()
+                        .padding(10)
+                    
+                }
                 
-                
-                
+                Text (playerName)
+                    .font(.headline)
+                Text ("""
+Pos:\(Pos) G: \(G) PTS: \(PTS)
+TRB: \(TRB) AST: \(AST)
+""")
+                Text (caption)
+                    .padding()
                 
             }
-            
-            Text ("Michael Jordan")
-                .font(.headline)
-            Text ("""
-Pos: SG G: 1072 PTS: 30.1
-TRB: 6.2 AST: 5.3
-""")
-            Text ("Micheal Jordan is regarded as one of the best NBA players of all time, he won 6 titles.")
-                .padding()
-            
         }
-    }
-    
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
+        
+        struct ContentView_Previews: PreviewProvider {
+            static var previews: some View {
+                NavigationView {
+                    TradingCard(imageOne: "Michael Jordan", playerName: "michael Jordan", Pos: "SG", G: "1072", PTS: "30.1", TRB: "6.2", AST: "5.3", caption: "Micheal Jordan is regarded as one of the best NBA players of all time, he won 6 titles.")
+                }
+            }
         }
-    }
 }
